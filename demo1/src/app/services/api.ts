f846545e5b562122f604e36d92b45df74ef2d03c
unknown <ravindar.balla@gmail.com>
2026-02-01 00:09:10 -0500

    conflict resovled for api

--- demo1/src/app/services/api.ts
+++ demo1/src/app/services/api.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  private apiData = inject(HttpClient);
-  url = 'https://jsonplaceholder.typicode.com/users';
-  constructor() {
-    console.log('Fetching data from API:', this.url);
-  }
+  private http = inject(HttpClient);
+  private url = 'http://localhost:3000/api/users';
+
+  // 1. The "Data Store" - Everyone listens to this
+  userList = signal<any[]>([]);
+
+  // 2. The Fetcher - Updates the signal
   fetchData() {
-    return this.apiData.get(this.url);
+    this.http.get(this.url).subscribe((data) => {
+      this.userList.set(data as any[]); // This updates the UI everywhere
+    });
+  }
+
+  // 3. The Poster - Returns observable so component knows when it's done
+  postData(newUser: any) {
+    return this.http.post(this.url, newUser);
   }
 }
